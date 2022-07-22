311f44a1cd53370e206635edcce6f9d45df57419
Talley Lambert <talley.lambert@gmail.com>
2022-07-22 12:45:15 -0400

    docs: add note

--- src/pydantic_generics/fields.py
+++ src/pydantic_generics/fields.py
@@ -28,5 +28,7 @@ def generic_types_allowed(field: pydantic.fields.ModelField) -> Iterator[None]:
 class ModelField(pydantic.fields.ModelField):
     def populate_validators(self) -> None:
         with patched_make_arbitrary_type_validator():
+            # XXX: not sure I like this, the alternative would be to just say
+            # "you have to use arbitrary_types_allowed if you want to use this"
             with generic_types_allowed(self):
                 super().populate_validators()
