7632e67485da9e65a1f0ca009dd9c78e99c7c16d
Lorenzo Gaifas <brisvag@gmail.com>
2022-07-25 16:18:57 +0200

    some broken tests

--- tests/test_pydantic_generics.py
+++ tests/test_pydantic_generics.py
@@ -1,33 +1,72 @@
-from typing import Any, Generic, Sequence, TypeVar, get_origin
+from typing import List, MutableSequence, Any, get_origin, MutableSet, MutableMapping, TypeVar
 
 import pytest
 
 from pydantic_generics import BaseModel, create_model
 
 T = TypeVar("T")
+U = TypeVar("U")
 
 
-class MyGeneric(Generic[T]):
-    def __init__(self, value: T):
-        self.value = value
+class Wrap:
+    def __init__(self, v):
+        self.v = v
+
+    def __getattr__(self, name):
+        return getattr(self.v, name)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.v})'
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.v
+
+    @classmethod
+    def v(cls, v):
+        return v
 
 
-class MyGenericSequence(Sequence[T]):
-    def __init__(self, data: Sequence[T]):
-        self._data = list(data)
+class MyList(List[T], Wrap):
+    pass
 
-    def __getitem__(self, index: int) -> T:  # type: ignore
-        return self._data[index]
 
-    def __len__(self) -> int:
-        return len(self._data)
+class MySeq(MutableSequence[T], Wrap):
+    __delitem__ = None
+    __getitem__ = None
+    __len__ = None
+    __setitem__ = None
+    insert = None
+    pass
+
+
+class MySet(MutableSet[T], Wrap):
+    __contains__ = None
+    __iter__ = None
+    __len__ = None
+    add = None
+    discard = None
+    pass
+
+
+class MyDict(MutableMapping[T, U], Wrap):
+    __delitem__ = None
+    __getitem__ = None
+    __len__ = None
+    __iter__ = None
+    __setitem__ = None
+    pass
 
 
 CASES = [
-    (MyGeneric, 1),
-    # (MyGeneric[int], 1),
-    (MyGenericSequence, [1]),
-    # (MyGenericSequence[int], [1]),
+    (MyList, [1]),
+    # (MyList[str], [1]),
+    (MySeq, [1]),
+    # (MySeq[str], [1]),
+    (MySet, {1}),
+    # (MySeq[str], {1}),
+    (MyDict, {1: 2}),
+    # (MyDict[str, str], {1: 2}),
 ]
 
 
