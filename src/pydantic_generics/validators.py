fd2782188543ca2b8647a2972333af853b8b3506
Lorenzo Gaifas <brisvag@gmail.com>
2022-07-26 15:30:02 +0200

    the solution to everything

--- src/pydantic_generics/validators.py
+++ src/pydantic_generics/validators.py
@@ -1,3 +1,4 @@
+from itertools import zip_longest
 from typing import Any, Callable, Type, TypeVar
 
 import pydantic.errors
@@ -24,3 +25,58 @@ def simple_casting_validator(type_: Type[T]) -> Callable[[T], T]:
             ) from e
 
     return arbitrary_type_validator
+
+
+class Missing:
+    pass
+
+
+def element_casting_validator(field) -> Callable[[T], T]:
+    def cast_elements(v: Any) -> T:
+        if not field.sub_fields:
+            return v
+
+        result = []
+        if len(field.sub_fields) == 1:
+            f = field.sub_fields[0]
+            for i, v_ in enumerate(v):
+                r, e = f.validate(v_, {}, loc=i)
+                if e:
+                    raise CannotCastError(type=f.type_, error='')
+                result.append(r)
+            return result
+        else:
+            for i, (v_, f) in enumerate(zip_longest(v, field.sub_fields, fillvalue=Missing)):
+                if v_ is Missing or f is Missing:
+                    raise ValueError('args must be either a single one, or as many as there are elements')
+                r, e = f.validate(v_, {}, loc=i)
+                if e:
+                    raise CannotCastError(type=f.type_, error='')
+                result.append(r)
+            return result
+
+    return cast_elements
+
+
+def mapping_casting_validator(field) -> Callable[[T], T]:
+    def cast_elements(v: Any) -> T:
+        if not field.sub_fields:
+            return v
+
+        if len(field.sub_fields) != 2:
+            raise ValueError('must pass 2 fields to mapping')
+
+        result = {}
+        for i, (k_, v_) in enumerate(v.items()):
+            f_key = field.sub_fields[0]
+            k, e = f_key.validate(k_, {}, loc=i)
+            if e:
+                raise CannotCastError(type=f_key.type_, error='')
+            f_val = field.sub_fields[1]
+            v, e = f_val.validate(v_, {}, loc=i)
+            if e:
+                raise CannotCastError(type=f_val.type_, error='')
+            result[k] = v
+        return result
+
+    return cast_elements
--- tests/test_pydantic_generics.py
+++ tests/test_pydantic_generics.py
@@ -8,6 +8,9 @@ from typing import (
     Sequence,
     TypeVar,
     get_origin,
+    Union,
+    Literal,
+    Optional,
 )
 
 import pytest
@@ -18,38 +21,59 @@ T = TypeVar("T")
 U = TypeVar("U")
 
 
-class MyGeneric(Generic[T]):
-    def __init__(self, value: T):
-        self.value = value
-
-
-class MyGenericSequence(Sequence[T]):
-    def __init__(self, data: Sequence[T]):
-        self._data = list(data)
-
-    def __getitem__(self, index: int) -> T:  # type: ignore
-        return self._data[index]
-
-    def __len__(self) -> int:
-        return len(self._data)
-
-
-class _CastMixin:
+class _ClassValidatorMixin:
     @classmethod
     def __get_validators__(cls):
         yield cls.v
 
     @classmethod
     def v(cls, v):
-        return cls(v)
+        # not a coercing validator!
+        return v
 
 
-class MyList(List[T]):
+class _ReprMixin:
+    def __repr__(self):
+        return f'{self.__class__.__name__}({repr(self.v)})'
+
+
+class MyGeneric(_ReprMixin, Generic[T]):
+    def __init__(self, value: T):
+        self.v = value
+
+
+class MyValidatingGeneric(_ClassValidatorMixin, MyGeneric[T]):
+    pass
+
+
+class MyGenericSequence(_ReprMixin, Sequence[T]):
+    def __init__(self, data: Sequence[T]):
+        self.v = list(data)
+
+    def __getitem__(self, index: int) -> T:  # type: ignore
+        return self.v[index]
+
+    def __len__(self) -> int:
+        return len(self.v)
+
+
+class MyValidatingGenericSequence(_ClassValidatorMixin, MyGenericSequence[T]):
+    pass
+
+
+class MyList(_ReprMixin, List[T]):
     def __init__(self, v):
-        self.v = v
+        self.v = list(v)
+
+    def __iter__(self):
+        yield from self.v
 
 
-class MyMutableSequence(MutableSequence[T]):
+class MyValidatingList(_ClassValidatorMixin, MyList[T]):
+    pass
+
+
+class MyMutableSequence(_ReprMixin, MutableSequence[T]):
     __delitem__ = None
     __getitem__ = None
     __len__ = None
@@ -57,29 +81,45 @@ class MyMutableSequence(MutableSequence[T]):
     insert = None
 
     def __init__(self, v):
-        self.v = v
+        self.v = list(v)
 
 
-class MyMutableSet(MutableSet[T]):
+class MyValidatingMutableSequence(_ClassValidatorMixin, MyMutableSequence[T]):
+    pass
+
+
+class MyMutableSet(_ReprMixin, MutableSet[T]):
     __contains__ = None
-    __iter__ = None
     __len__ = None
     add = None
     discard = None
 
     def __init__(self, v):
-        self.v = v
+        self.v = set(v)
+
+    def __iter__(self):
+        yield from self.v
 
 
-class MyMutableMapping(MutableMapping[T, U]):
+class MyValidatingMutableSet(_ClassValidatorMixin, MyMutableSet[T]):
+    pass
+
+
+class MyMutableMapping(_ReprMixin, MutableMapping[T, U]):
     __delitem__ = None
     __getitem__ = None
     __len__ = None
-    __iter__ = None
     __setitem__ = None
 
     def __init__(self, v):
-        self.v = v
+        self.v = dict(v)
+
+    def __iter__(self):
+        yield from self.v
+
+
+class MyValidatingMutableMapping(_ClassValidatorMixin, MyMutableMapping[T]):
+    pass
 
 
 CASES = [
@@ -91,26 +131,93 @@ CASES = [
     (MyMutableSequence, [1]),
     (MyMutableSet, {1}),
     (MyMutableMapping, {1: 2}),
-]
-
-
-PARAMETRIZED_CASES = [
-    (MyGeneric[int], 1),
-    (MyGenericSequence[int], [1]),
-    (MyGeneric[int], 1),
-    (MyGenericSequence[int], [1]),
-    (MyMutableSequence[str], [1]),
-    (MyList[str], [1]),
-    (MyMutableMapping[str, str], {1: 2}),
-    (MyMutableSequence[str], {1}),
+    (MyValidatingGeneric, 1),
+    (MyValidatingGenericSequence, [1]),
+    (MyValidatingGeneric, 1),
+    (MyValidatingGenericSequence, [1]),
+    (MyValidatingList, [1]),
+    (MyValidatingMutableSequence, [1]),
+    (MyValidatingMutableSet, {1}),
+    (MyValidatingMutableMapping, {1: 2}),
+    # input of different type that can be coerced
+    (MyGenericSequence, {1}),
+    (MyList, {1}),
+    (MyMutableSequence, {1}),
+    (MyMutableSet, [1]),
+    (MyValidatingGenericSequence, {1}),
+    (MyValidatingList, {1}),
+    (MyValidatingMutableSequence, {1}),
+    (MyValidatingMutableSet, [1]),
 ]
 
 
 @pytest.mark.parametrize("field, value", CASES)
-def test_something(field: type, value: Any) -> None:
+def test_simple_generics(field: type, value: Any) -> None:
     Model = create_model("Model", x=(field, ...))
     assert issubclass(Model, BaseModel)
     instance = Model(x=value)
     attr = getattr(instance, "x")
     custom_type = get_origin(field) or field
     assert isinstance(attr, custom_type)
+
+
+PARAMETRIZED_CASES = [
+    (MyGeneric[int], 1, 1),
+    (MyGenericSequence[int], [1], [1]),
+    (MyGeneric[int], 1, 1),
+    (MyGenericSequence[int], [1], [1]),
+    (MyMutableSet[int], {1}, {1}),
+    (MyValidatingGeneric[int], 1, 1),
+    (MyValidatingGenericSequence[int], [1], [1]),
+    (MyValidatingGeneric[int], 1, 1),
+    (MyValidatingGenericSequence[int], [1], [1]),
+    (MyValidatingMutableSet[int], {1}, {1}),
+    # coerce element type
+    (MyMutableSequence[str], [1], ['1']),
+    (MyList[str], [1], ['1']),
+    (MyMutableMapping[str, str], {1: 2}, {'1': '2'}),
+    (MyMutableSet[str], {1}, {'1'}),
+    (MyMutableSequence[str], {1}, ['1']),
+    (MyValidatingMutableSequence[str], [1], ['1']),
+    (MyValidatingList[str], [1], ['1']),
+    (MyValidatingMutableMapping[str, str], {1: 2}, {'1': '2'}),
+    (MyValidatingMutableSet[str], {1}, {'1'}),
+    (MyValidatingMutableSequence[str], {1}, ['1']),
+    # coerce container type as well
+    (MyMutableSequence[str], {1}, ['1']),
+    (MyList[str], {1}, ['1']),
+    (MyMutableSet[str], [1], {'1'}),
+    (MyValidatingMutableSequence[str], {1}, ['1']),
+    (MyValidatingList[str], {1}, ['1']),
+    (MyValidatingMutableSet[str], [1], {'1'}),
+]
+
+
+@pytest.mark.parametrize("field, value, expected", PARAMETRIZED_CASES)
+def test_parametrized_generics(field: type, value: Any, expected: Any) -> None:
+    Model = create_model("Model", x=(field, ...))
+    assert issubclass(Model, BaseModel)
+    instance = Model(x=value)
+    attr = getattr(instance, "x")
+    custom_type = get_origin(field) or field
+    assert isinstance(attr, custom_type)
+    assert attr.v == expected
+
+
+OTHER_CASES = [
+    (Union[str, float], 1.0, 1.0, float),
+    (Union[float, str], '1', '1', str),
+    (Optional[int], None, None, type(None)),
+    (Optional[int], 1, 1, int),
+    (Literal[1], 1, 1, int)
+]
+
+
+@pytest.mark.parametrize("field, value, expected, expected_type", OTHER_CASES)
+def test_other_types(field: type, value: Any, expected: Any, expected_type: type) -> None:
+    Model = create_model("Model", x=(field, ...))
+    assert issubclass(Model, BaseModel)
+    instance = Model(x=value)
+    attr = getattr(instance, "x")
+    assert attr == expected
+    assert type(attr) is expected_type
