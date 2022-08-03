b568c98fc869df1544bf7f887d7ca9861fefeeb5
Lorenzo Gaifas <brisvag@gmail.com>
2022-08-03 17:15:07 +0200

    slightly less ugly fix

--- src/pydantic_generics/main.py
+++ src/pydantic_generics/main.py
@@ -1,4 +1,3 @@
-from contextlib import contextmanager
 from typing import Any, Type, no_type_check
 
 import pydantic.main
@@ -6,16 +5,7 @@ import pydantic.main
 from .monkeypatch import patched_pydantic_base_model, patched_pydantic_model_field, patched_dataclass_validator
 
 
-is_basemodel = False
-
-
-@contextmanager
-def is_basemodel():
-    # allows us to declare BaseModel without triggering the arbitrary_types_allowed error
-    global is_basemodel
-    is_basemodel = True
-    yield
-    is_basemodel = False
+_is_base_model_class_defined = False
 
 
 class ModelMetaclass(pydantic.main.ModelMetaclass):
@@ -23,14 +13,16 @@ class ModelMetaclass(pydantic.main.ModelMetaclass):
     def __new__(cls, name, bases, namespace, **kwargs):
         with patched_pydantic_model_field(), patched_dataclass_validator():
             new_cls = super().__new__(cls, name, bases, namespace, **kwargs)
-            if not is_basemodel and not new_cls.__config__.arbitrary_types_allowed:
+            if _is_base_model_class_defined and not new_cls.__config__.arbitrary_types_allowed:
                 raise ValueError('arbitrary_types_allowed must be True for pydantic_generics to work')
             return new_cls
 
 
-with is_basemodel():
-    class BaseModel(pydantic.main.BaseModel, metaclass=ModelMetaclass):
-        pass
+class BaseModel(pydantic.main.BaseModel, metaclass=ModelMetaclass):
+    pass
+
+
+_is_base_model_class_defined = True
 
 
 def create_model(__model_name: str, **kwargs: Any) -> Type["BaseModel"]:
